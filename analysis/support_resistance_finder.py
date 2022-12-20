8af78214efee82f877eca9d464868667d358ab68
Hamza <hamza.ahmedqureshi@hotmail.com>
2022-12-20 14:58:30 +1100

    added treadlines

--- analysis/support_resistance_finder.py
+++ analysis/support_resistance_finder.py
@@ -37,24 +37,27 @@ def combine_maximums(maximums, threshold=.2):
     '''
     merges nearby maximas into one. 
     higher threshold leads to more distinct maximas
+    the maxima object is a 4-tuple, consisting of the 
+    resistance value, start, end, and maximum point in the resistance level
     '''
     curr_maxima = maximums[0]
     combined_maximas = [[0, 0, 0]]
     span = max(maximums, key=lambda x: x[0])[0] - min(maximums, key=lambda x: x[0])[0]
     for maxima in maximums:
-        if abs(maxima[0] - curr_maxima[0]) > threshold * span :
-            combined_maximas.append([curr_maxima[0], combined_maximas[-1][-1], maxima[1]])
+        if abs(maxima[0] - curr_maxima[0]) > threshold * span:
+            combined_maximas.append([curr_maxima[0], combined_maximas[-1][-1], maxima[1], curr_maxima[1]])
             curr_maxima = maxima
         elif maxima[0] > curr_maxima[0]:
             curr_maxima = maxima
-    combined_maximas.append([curr_maxima[0], combined_maximas[-1][-1], curr_maxima[1]])
+    combined_maximas.append([curr_maxima[0], combined_maximas[-1][-1], curr_maxima[1], curr_maxima[1]])
 
     i = 1
     while i < len(combined_maximas) - 1:
         if abs(combined_maximas[i][0] - combined_maximas[i + 1][0]) < threshold * span:
             combined_maximas[i + 1] = [max(combined_maximas[i][0], combined_maximas[i + 1][0]), 
                                                             combined_maximas[i][1], 
-                                                            combined_maximas[i + 1][2]]
+                                                            combined_maximas[i + 1][2],
+                                                            max(combined_maximas[i], combined_maximas[i + 1], key=lambda x: x[0])[3]]
             del combined_maximas[i]
         else:
             i+=1
@@ -67,31 +70,47 @@ def combine_minimums(minimums, threshold=0.2):
     '''
     merges nearby minimas into one. 
     higher threshold leads to more distinct minimas
+    the minima object is a 4-tuple, consisting of the 
+    resistance value, start, end, and minimum point in the resistance level
     '''
 
     curr_minima = minimums[0]
-    combined_minimas = [[0, 0, 0]]
+    combined_minimas = [[0, 0, 0, 0]]
     span = max(minimums, key=lambda x: x[0])[0] - min(minimums, key=lambda x: x[0])[0]
     for minima in minimums:
         if abs(minima[0] - curr_minima[0]) > threshold * span / 2:
-            combined_minimas.append([curr_minima[0], combined_minimas[-1][-1], minima[1]])
+            combined_minimas.append([curr_minima[0], combined_minimas[-1][-1], minima[1], curr_minima[1]])
             curr_minima = minima
         elif minima[0] < curr_minima[0]:
             curr_minima = minima
-    combined_minimas.append([curr_minima[0], combined_minimas[-1][-1], curr_minima[1]])
+    combined_minimas.append([curr_minima[0], combined_minimas[-1][-1], curr_minima[1], curr_minima[1]])
 
     i = 1
     while i < len(combined_minimas) - 1:
         if abs(combined_minimas[i][0] - combined_minimas[i + 1][0]) < threshold * span:
             combined_minimas[i + 1] = [min(combined_minimas[i][0], combined_minimas[i + 1][0]), 
                                                             combined_minimas[i][1], 
-                                                            combined_minimas[i + 1][2]]
+                                                            combined_minimas[i + 1][2],
+                                                            min(combined_minimas[i], combined_minimas[i + 1], key=lambda x: x[0])[3]]
             del combined_minimas[i]
         else:
             i+=1
     return combined_minimas[1:]
 
 
+def plot(data, minimums, maximums, treadlines=True, levels=True):
+
+    data.plot()
+    
+    if treadlines:
+        plot_treadlines(maximums)
+
+    if levels:
+        plot_points(data, minimums, maximums)
+    
+    plt.show()
+
+        
 def plot_points(df, minimums, maximums):
 
     '''
@@ -100,11 +119,11 @@ def plot_points(df, minimums, maximums):
 
     minimums[-1][-1] = maximums[-1][-1] = df.shape[0]
 
-    df.plot()
     for minima in minimums:
         plt.hlines(minima[0], minima[1], minima[2], colors="red")
     for maxima in maximums:
         plt.hlines(maxima[0], maxima[1], maxima[2], colors="green")
+
     plt.show()
 
 
@@ -113,7 +132,7 @@ def find_support_resistance(data):
     '''
     find the resistance and support levels'''
 
-    threshold = 0.3
+    threshold = 0.17
     minimums, maximums = find_poi(data)
 
     combined_minimums = combine_minimums(minimums, threshold)
@@ -123,24 +142,35 @@ def find_support_resistance(data):
     while len(combined_maximums) > min_len:
         threshold += 0.001
         combined_maximums = combine_maximums(combined_maximums, threshold)
+        if len(combined_maximums) < 2:
+            print("oops")
+            combined_maximums = combine_maximums(combined_maximums, threshold - 0.001)
+            break
 
     min_len = min(len(combined_minimums), len(combined_maximums))
 
     while len(combined_minimums) > min_len:
         threshold += 0.01
         combined_minimums = combine_minimums(minimums, threshold)
+        if len(combined_minimums) < 2:
+            combined_minimums = combine_minimums(combined_minimums, threshold - 0.001)
+            break
 
     print(threshold)
+    plot(data, combined_minimums, combined_maximums)
     
 
-    plot_points(data, combined_minimums, combined_maximums)
+
+def plot_treadlines(maximums):
+
+    plt.plot([x[3] for x in maximums], [y[0] for y in maximums], color='hotpink', linestyle='dashed')
 
 
 if __name__ == "__main__":
 
-    df = pd.read_csv("last_90_days.csv", header=0)
+    df = pd.read_csv("last_2_years.csv", header=0)
 
-    # df = df.sort_values(by="date").reset_index()
+    df = df.sort_values(by="date").reset_index()
     df = df["close"]
 
     find_support_resistance(df)
--- data/gather_data.py
+++ data/gather_data.py
@@ -4,21 +4,24 @@ from fetch_data import fetch_data
 
 RECORDS_LIMIT = 300
 GRANULARITY = 86400
-CURRENCY = "BTC/USD"
+
+COIN = "DOGE"
+CURRENCY = "USD"
+CODE = f'{COIN}/{CURRENCY}'
 
 # Minimum Amount of Data Required
-DAYS = 365 * 2
+DAYS = 90
 HOURS = 0
 
 num_seconds = (DAYS * 24 + HOURS) * 3600 # oldest data required in seconds
 iterations_required = int(((num_seconds / GRANULARITY) / RECORDS_LIMIT))
 
-data = fetch_data(CURRENCY, GRANULARITY)
+data = fetch_data(CODE, GRANULARITY)
 for i in range(iterations_required):
     end = data.iloc[-1, 0] - GRANULARITY
     start = end - RECORDS_LIMIT * GRANULARITY
 
-    df2 = fetch_data(CURRENCY, GRANULARITY, start, end)
+    df2 = fetch_data(CODE, GRANULARITY, start, end)
     data = pd.concat([data, df2], ignore_index=True)
 
-data.to_csv(f'last_{DAYS}_days.csv')
+data.to_csv(f'{COIN}_last_{DAYS}_days.csv')
